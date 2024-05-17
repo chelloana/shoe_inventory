ed38609d027800bb50fb43cd276961f63e6bec7e
chelloana <77709307+chelloana@users.noreply.github.com>
2024-05-17 22:01:24 +0700

    Update server.js

--- server.js
+++ server.js
@@ -14,5 +14,5 @@ app.get("/login.html", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`server listening on port ${PORT}...`);
+    console.log(`Server is running on port ${PORT}...`);
 });
