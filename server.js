6450051ef3ccd7c11c60455ddb0273af32949e95
chelloana <77709307+chelloana@users.noreply.github.com>
2024-05-17 19:24:21 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cors = require('cors'); // Import paket cors
 const app = express();
 const path = require('path');
 const sequelize = require('./config/db.config');
@@ -8,7 +7,14 @@ const userRoutes = require('./routes/userRoutes');
 
 // Middleware
 app.use(express.json());
-app.use(cors()); // Gunakan middleware cors di sini
+
+// Middleware for CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 // Routes
 app.use('/api', authRoutes);
