3643c08233d2f41aa5430c1e1affc4310cec4f37
chelloana <77709307+chelloana@users.noreply.github.com>
2024-05-17 22:34:13 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,17 +2,13 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
-// Serve index.html saat permintaan root URL ("/")
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
-});
-
-// Serve login.html saat permintaan "/login.html"
-app.get("/login.html", (req, res) => {
+    // Membaca file index.html dari direktori yang sama dengan server.js
     res.sendFile(path.join(__dirname, "login.html"));
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}...`);
+    console.log(`server listening on port ${PORT}...`);
 });
