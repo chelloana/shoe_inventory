c6246ac1460d8aed70ba958922ab3a5ea953ebab
chelloana <77709307+chelloana@users.noreply.github.com>
2024-05-17 18:51:20 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ const shoeRoutes = require('./routes/shoeRoutes');
 // Middleware
 app.use(express.json());
 
+// Middleware for CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 // Routes
 app.use('/api', shoeRoutes);
 
